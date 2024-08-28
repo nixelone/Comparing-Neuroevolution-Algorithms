d677863db7adffa641a6a4c4221a2150c093ff95
Marko Hlavaty <marko.hlavaty@gmail.com>
2024-08-28 18:55:09 +0200

    Add docstrings to config_processor.py

--- config_processor.py
+++ config_processor.py
@@ -1,3 +1,8 @@
+"""
+This module contains functions that extract information from
+config files and dataclasses that store this information
+"""
+
 import re
 import tempfile
 import neat
@@ -6,11 +11,19 @@ from dataclasses import dataclass
 
 
 class DuplicateOptionError(Exception):
+    """
+    Exception that is raised when a parameter is
+    listed more than once in a config file
+    """
     pass
 
 
 @dataclass
 class FixedTopologyNetworkConfig:
+    """
+    Dataclass that holds information about
+    a fixed topology network
+    """
     num_inputs: int
     num_outputs: int
     hidden_layers: int
@@ -21,6 +34,10 @@ class FixedTopologyNetworkConfig:
 
 @dataclass
 class CMAESPopulationConfig:
+    """
+    Dataclass that holds information about
+    an instance of CMA-ES algorithm
+    """
     mu: int
     lambda_: int
     sigma: float
@@ -28,11 +45,25 @@ class CMAESPopulationConfig:
 
 @dataclass
 class DifferentialEvolutionConfig:
+    """
+    Dataclass that holds information about an
+    instance of Differential Evolution algorithm
+    """
     population_size: int
     bound: float
 
 
 def extract_value(variable_name, variable_type, config_file):
+    """
+    Extracts value corresponding to a given name from a given
+    config file and tries to return it as specified type
+
+    Raises an exception when the item is not listed
+    or if it is listed more than once
+
+    An exception is also raised is the listed value
+    cannot be converted to the specified type
+    """
     with open(config_file, 'r') as f:
         config_text = f.read()
 
@@ -46,7 +77,16 @@ def extract_value(variable_name, variable_type, config_file):
     return search_result
 
 
-def check_line_name(names, line):  # names can be either a string (one name) or a list of strings (multiple names)
+def check_line_name(names, line):
+    """
+    Checks for name of the variable that is in the given string,
+    returns True if at least one of the specified names
+    is the string variable name
+
+    Names can be either a string (one name) or a list of strings (multiple names)
+    """
+    # handles inputs that are given in string format
+    # instead of a list of strings
     if type(names) == str:
         names = [names]
 
@@ -58,6 +98,10 @@ def check_line_name(names, line):  # names can be either a string (one name) or 
 
 
 def create_hyperneat_substrate_config(config_file):
+    """
+    Creates and returns FixedTopologyNetworkConfig for
+    HyperNEAT substrate based on the given config file
+    """
     substrate_num_inputs = extract_value('substrate_num_inputs', int, config_file)
     substrate_num_outputs = extract_value('substrate_num_outputs', int, config_file)
     substrate_hidden_layers = extract_value('substrate_hidden_layers', int, config_file)
@@ -77,6 +121,10 @@ def create_hyperneat_substrate_config(config_file):
 
 
 def create_hyperneat_cppn_config(config_file, n_substrate_dimensions):
+    """
+    Creates and returns config for HyperNEAT CPPN that is evolved
+    by NEAT algorithm based on the specified config file
+    """
     with tempfile.NamedTemporaryFile(mode='w+t', delete=False) as temp_file:
         temp_filename = temp_file.name
 
@@ -105,6 +153,10 @@ def create_hyperneat_cppn_config(config_file, n_substrate_dimensions):
 
 
 def create_neft_network_config(config_file):
+    """
+    Creates and returns FixedTopologyNetworkConfig for fixed
+    topology neural network based on the given config file
+    """
     num_inputs = extract_value('num_inputs', int, config_file)
     num_outputs = extract_value('num_outputs', int, config_file)
     hidden_layers = extract_value('hidden_layers', int, config_file)
@@ -124,6 +176,10 @@ def create_neft_network_config(config_file):
 
 
 def create_cmaes_config(config_file):
+    """
+    Creates and returns CMAESPopulationConfig for
+    CMA-ES algorithm based on the given config file
+    """
     mu = extract_value('mu', int, config_file)
     lambda_ = extract_value('lambda', int, config_file)
     sigma = extract_value('sigma', float, config_file)
@@ -137,6 +193,10 @@ def create_cmaes_config(config_file):
 
 
 def create_de_config(config_file):
+    """
+    Creates and returns DifferentialEvolutionConfig for
+    differential evolution based on the given config file
+    """
     population_size = extract_value('population_size', int, config_file)
     bound = extract_value('bound', float, config_file)
 
