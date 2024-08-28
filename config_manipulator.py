807b80b2a29f1124bb612d5c29a9b60f629c2037
Marko Hlavaty <marko.hlavaty@gmail.com>
2024-08-28 18:26:59 +0200

    Add docstrings to config_manipulator.py

--- config_manipulator.py
+++ config_manipulator.py
@@ -1,12 +1,25 @@
+"""
+This module contains functions that can be used for manipulation with config files
+"""
+
 import os
 import shutil
 
 
 class NonExistentConfigFileError(Exception):
+    """
+    Exception that is raised when there is attempt at finding
+    a config file that does not exist in the file system
+    """
     pass
 
 
 def get_config_file_path(environment_name, algorithm_name):
+    """
+    Assembles and returns config file path corresponding to given environment and algorithm
+
+    Raises an exception if the file does not exist
+    """
     file_path = os.path.join('config_files', environment_name, f'{algorithm_name}.txt')
 
     if not os.path.exists(file_path):
@@ -16,6 +29,12 @@ def get_config_file_path(environment_name, algorithm_name):
 
 
 def restore_default_config(environment_name, algorithm_name):
+    """
+    Restores config file corresponding to given environment and algorithm
+
+    Default config file is taken from default_config_files directory
+    """
+
     default_file_path = os.path.join('default_config_files', environment_name, f'{algorithm_name}.txt')
     file_path = os.path.join('config_files', environment_name, f'{algorithm_name}.txt')
 
@@ -26,6 +45,12 @@ def restore_default_config(environment_name, algorithm_name):
 
 
 def restore_all_default_configs():
+    """
+    Restores all config files
+
+    Default config files are taken from default_config_files directory
+    """
+
     DEFAULT_CONFIG_DIRECTORY = 'default_config_files'
     CONFIG_DIRECTORY = 'config_files'
 
